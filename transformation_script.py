7e92f93bd7ce88ad9cbdb03d682e9eadb7b3632d
Ngozi Harrison <ngozih@g.ucla.edu>
2024-07-25 11:49:12 -0700

    updated comments

--- transformation_script.py
+++ transformation_script.py
@@ -42,7 +42,7 @@ def transform_input(row):
             for (name, lang) in alt_names:
                 data["alt_name"].append({"lang": lang, "value": name})
 
-    # Check to see if there is a date normalized
+    # Check to see if there is a date normalized with before and after
     if "/" in row["Dates.normalized"]:
         not_before = row["Dates.normalized"].split("/")[0].rjust(4, "0")
         not_after = row["Dates.normalized"].split("/")[1].rjust(4, "0")
@@ -57,30 +57,33 @@ def transform_input(row):
     # Check LOC
     if pd.isnull(row["LOC"]) == False:
          data["rel_con"].append({"label": row["Name.LOC"], "uri": row["LOC"], "source": "LoC"})
-    
+    # Check HAF
     if pd.isnull(row["HAF"]) == False:
          data["rel_con"].append({"label": row["Name.HAF"], "uri": row["HAF"], "source": "HAF"})
-
+    # Check Syriaca
     if pd.isnull(row["Syriaca"]) == False:
          data["rel_con"].append({"label": row["Name.Syriaca"], "uri": row["Syriaca"], "source": "Syriaca"})
-
+    # Check Pinakes
     if pd.isnull(row["Pinakes"]) == False:
          data["rel_con"].append({"label": row["Name.Pinakes"], "uri": row["Pinakes"], "source": "Pinakes"})
-
+    # Check Notes
     if pd.isnull(row["Notes"]) == False:
          data["Notes"].append({"value": row["Notes"]})
 
-    
+    # Check to see if json directory exists if not create it
     if not os.path.exists("json"):
         os.makedirs("json")
+
+    # Export JSON
     with open(f'json/{local_id}.json', 'w+') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
     
-
+# Check to see if user entered path to csv file
 try:
     csv_file = pd.read_csv(sys.argv[1])    
 except Exception as e:
     print("ERROR: Please enter the path to the CSV you would like to transform", str(e))
 
+# run transformation script
 for i, row in csv_file.iterrows():
         transform_input(row)
